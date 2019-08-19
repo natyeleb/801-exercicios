9dc63768c0c3953252591b7834493e26b06bdaab
natyele <natyelebertolanii@gmail.com>
2019-08-19 16:37:31 -0300

    exercicios de python

---
+++ exercicio2.py
@@ -0,0 +1,7 @@
+tel = input('digite o seu telefone:')
+
+if tel [0] == "9" and len(tel) == "9":
+   print("telefone valido")
+
+else: 
+    print("telefone invalido")
---
+++ exercicios.py
@@ -0,0 +1,11 @@
+numero = float(input('Digite um número para saber se é par ou impar:'))
+
+
+if numero % 2 == 0:
+    print('numero é par')
+
+else:
+    print('numero é impar')
+
+
+
